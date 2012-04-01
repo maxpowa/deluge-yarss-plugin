f6aab7765452db213f435ba7ce1d0a9110a9abb8
bendikro <bendikro@gmail.com>
2012-04-01 18:43:29 +0200

    Updated setup.py with correct version

--- setup.py
+++ setup.py
@@ -42,14 +42,15 @@ from setuptools import setup
 __plugin_name__ = "YaRSS2"
 __author__ = "Bro"
 __author_email__ = "bro.development@gmail.com"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __url__ = ""
 __license__ = "GPLv3"
 __description__ = "Yet another RSS 2"
 __long_description__ = """
 Yet another RSS 2, a simple RSS plugin for Deluge, based on 
 YaRSS written by Camillo Dell'mour <cdellmour@gmail.com>. 
-Last updated on 2012-03-27, tested with Deluge 1.3.4.
+Last updated on 2012-04-01, tested with Deluge 1.3.4.
+Requires PyGTK 2.24
 """
 __pkg_data__ = {__plugin_name__.lower(): ["gtkui/*", "data/*", "lib/*"]}
 
