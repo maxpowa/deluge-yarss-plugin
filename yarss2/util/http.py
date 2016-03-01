8b942e455d6f9e590345a831b26861f6ec90399a
bendikro <bro.devel+yarss2@gmail.com>
2016-03-01 19:42:51 +0100

    Fix #19: Incorrect, URL encoded download link for .torrent file

--- yarss2/util/http.py
+++ yarss2/util/http.py
@@ -66,7 +66,7 @@ def url_fix(s, charset='utf-8'):
     if isinstance(s, unicode):
         s = s.encode(charset, 'ignore')
     scheme, netloc, path, qs, anchor = urlparse.urlsplit(s)
-    path = urllib.quote(path, '/%')
+    path = urllib.quote(path, safe="%/:=&?~#+!$,;'@()*[]")
     qs = urllib.quote_plus(qs, ':&=')
     return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
 
